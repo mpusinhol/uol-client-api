6d8b9ae6d310aea446124045c4fda041e741e7ea
Matheus Pusinhol <mpusinhol@gmail.com>
2019-01-16 01:12:44 -0200

    First commit

---
+++ src/test/java/com/mpusinhol/clientapi/ClientApiApplicationTests.java
@@ -0,0 +1,17 @@
+package com.mpusinhol.clientapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ClientApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
